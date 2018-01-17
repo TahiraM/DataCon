b9a66c812401569f15e89be2b8ec6dd365d8aa0e
Tahira Mahmood <tahiramahmood@Tahiras-MacBook-Pro.local>
2018-01-17 13:50:16 +0000

    comments added

--- Conversions/CsvConv.cs
+++ Conversions/CsvConv.cs
@@ -12,9 +12,9 @@ namespace Conversions
     {
         public static DataTable ReadFile()
         {
-            DataTable dtCsv = new DataTable();
+            DataTable dtCsv = new DataTable(); //create a datatable to store the CSV file info- to make it easier to covert to json
             string Fulltext;
-            using(StreamReader sr = new StreamReader("Deal.csv"))
+            using(StreamReader sr = new StreamReader("Deal.csv")) //steamreader will read through documents
             {
                 while (!sr.EndOfStream)
                 {
@@ -23,32 +23,32 @@ namespace Conversions
 
                     for (int i = 0; i < rows.Count(); i++)
                     {
-                        string[] rowValues = rows[i].Split("||"); //split each row with lne to get individual values  
+                        string[] rowValues = rows[i].Split("||"); //split each row with || to get individual values
                         {
-                            DataRow dh = dtCsv.NewRow();
+                            DataRow dh = dtCsv.NewRow(); //create 2 row types for datatable, for headers and content
                             DataRow dr = dtCsv.NewRow();
                             if (i == 0)
                             {
                                 for (int j = 0; j < rowValues.Count(); j++)
                                 {
-                                    dtCsv.Columns.Add(rowValues[j]);
-                                    dh[j] = rowValues[j].ToString();
+                                    dtCsv.Columns.Add(rowValues[j]); //look through the first line and see how many values are present = no of columns
+                                    dh[j] = rowValues[j].ToString(); //create the headers and add to table
                                 }
-                                dtCsv.Rows.Add(dh);
+                                dtCsv.Rows.Add(dh); //add to table
                             }
                             if (i>0)
                             {
                                 for (int k = 0; k < rowValues.Count(); k++)
                                 {
-                                    dr[k] = rowValues[k].ToString();
+                                    dr[k] = rowValues[k].ToString(); //look through remaining lines of data
                                 }
-                                dtCsv.Rows.Add(dr);
+                                dtCsv.Rows.Add(dr);//add remaining lines of data under correct header
                             }
                         }
                     }
                 }
             }
-            foreach (DataRow row in dtCsv.Rows)
+            foreach (DataRow row in dtCsv.Rows) //method to display table on console to check accuracy
             {
                 Console.WriteLine();
                 for(int x = 0; x < dtCsv.Columns.Count; x++)
@@ -56,41 +56,42 @@ namespace Conversions
                     Console.Write(row[x].ToString() + " ");
                 }
             }
-            DataTableToJSON(dtCsv);
+            DataTableToJSON(dtCsv); //calling method to covert to JSON
              return dtCsv;
         }
 
-        public static string DataTableToJSON(DataTable dtCsv)
+        public static string DataTableToJSON(DataTable dtCsv)//convert to JSON method
         {
-            var JSONString = new StringBuilder();
+            var JSONString = new StringBuilder();//Stringbuilder enables changes to data so if CSV data is altered it changes in JSON too
             if (dtCsv.Rows.Count > 0)
             {
-                JSONString.Append("[");
+                JSONString.Append("[");//start JSON file
                 for (int i = 0; i < dtCsv.Rows.Count; i++)
                 {
-                    JSONString.Append("{");
+                    JSONString.Append("{");//start first row of data
                     for (int j = 0; j < dtCsv.Columns.Count; j++)
                     {
-                        if (j < dtCsv.Columns.Count - 1)
+                        if (j < dtCsv.Columns.Count -1)
                         {
                             JSONString.Append("\"" + dtCsv.Columns[j].ColumnName.ToString() + "\":" + "\"" + dtCsv.Rows[i][j].ToString() + "\",");
-                        }
+                        }   //selecting the headers and matching them to values
                         else if (j == dtCsv.Columns.Count - 1)
                         {
                             JSONString.Append("\"" + dtCsv.Columns[j].ColumnName.ToString() + "\":" + "\"" + dtCsv.Rows[i][j].ToString() + "\"");
-                        }
+                        }   //filling in values for all rows
                     }
                     if (i == dtCsv.Rows.Count - 1)
                     {
-                    JSONString.Append("}");
+                    JSONString.Append("}");//end JSON file when all rows have been read
                     }
                     else
                     {
-                    JSONString.Append("},");
+                    JSONString.Append("},");//start a new row
                     }
                 }
-                JSONString.Append("]");
+                JSONString.Append("]");//end file
             }
+            //save output into a .json file 
             FileStream Sjson = new FileStream ("Deal.json", FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter addJ= new StreamWriter(Sjson);
             Console.SetOut(addJ);
--- Conversions/Program.cs
+++ Conversions/Program.cs
@@ -14,29 +14,7 @@ namespace Conversions
     {
         public static void Main(string[] args)
         {
-            /*using( var reader = new StreamReader("Deal.csv"))
-            {
-                List<string> one = new List<string>();
-                List<string> two = new List<string>();
-
-            
-            while(!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                var values = line.Split('|');
-                one.Add(values[0]);
-
-                var linetwo = reader.ReadLine();
-                var valtwo = linetwo.Split('|').Skip(1);
-                two.Add(values[0]);
-
-                Console.WriteLine(string.Join("\t", one.Cast<string>().ToArray()));
-                Console.WriteLine(string.Join("\t", two.Cast<string>().ToArray()));
-            }
-            }*/
-            CsvConv.ReadFile();
-            
-
+            CsvConv.ReadFile(); //calling method in CsvCon class
         }
     }
 }
